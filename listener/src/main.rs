1617e033dd436610cc46f4385421918fea456f41
akshay <akshay_111meher@hotmail.com>
2024-06-04 10:46:05 +0400

    Update

--- listener/src/main.rs
+++ listener/src/main.rs
@@ -61,12 +61,15 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let alt_generator_config_path = "../generator_config/generator_config.json".to_string();
     let file_content = fs::read_to_string(&generator_config_path)
         .or_else(|_| fs::read_to_string(&alt_generator_config_path))?;
+
+    println!("{}", &file_content);
     let config: Config = serde_json::from_str(&file_content)?;
 
     let runtime_config_path = "./generator_config/runtime_config.json".to_string();
     let alt_runtime_config_path = "../generator_config/runtime_config.json".to_string();
     let file_content = fs::read_to_string(&runtime_config_path)
         .or_else(|_| fs::read_to_string(&alt_runtime_config_path))?;
+    println!("{}", &file_content);
     let runtime_config: RuntimeConfig = serde_json::from_str(&file_content)?;
     let runtime_config = runtime_config.runtime_config;
 
