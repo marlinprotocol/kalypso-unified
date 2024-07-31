66fa418ae18e1787b1adade49240ff2fa3100373
akshay <akshay_111meher@hotmail.com>
2024-07-31 16:14:58 +0400

    Init ME integration checks

--- matching_engine/src/lib.rs
+++ matching_engine/src/lib.rs
@@ -5,7 +5,7 @@ use generator::{Request, RequestType};
 pub fn get_welcome_request<R>() -> Request<(), R> {
     Request {
         request_type: RequestType::GET,
-        service_endpoint: "/welcome".into(),
+        service_endpoint: "/api/test".into(),
         _marker: std::marker::PhantomData::<R>,
     }
 }
