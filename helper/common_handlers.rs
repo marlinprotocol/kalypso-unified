a12f5cf3b24ba9622f2b916e7cc14a55874d1ed0
akshay <akshay_111meher@hotmail.com>
2024-09-16 16:16:08 +0400

    sign address encrypted

--- helper/common_handlers.rs
+++ helper/common_handlers.rs
@@ -1,13 +1,133 @@
 use crate::response::response;
+use crate::secret_inputs_helpers::{decrypt_ecies, encrypt_ecies};
 use actix_web::http::StatusCode;
 use actix_web::web::Data;
 use actix_web::{post, web, Responder};
+use anyhow::Context;
+use ethers::abi::{encode, Token};
+use ethers::core::utils::keccak256;
 use ethers::prelude::*;
 use serde::{Deserialize, Serialize};
 use serde_json::{json, Value};
 use std::str::FromStr;
 use std::sync::{Arc, Mutex};
 use validator::Validate;
+
+#[derive(Serialize, Debug, Clone, Deserialize)]
+pub struct SCHPayload {
+    request: Vec<u8>,
+    response_key: Vec<u8>,
+    signature: Vec<u8>,
+}
+
+pub trait ToPayload<T> {
+    fn to_payload(&self, ecies_priv_key: &Vec<u8>) -> anyhow::Result<T>;
+}
+
+impl<T> ToPayload<T> for SCHPayload
+where
+    T: for<'de> Deserialize<'de>,
+{
+    fn to_payload(&self, ecies_priv_key: &Vec<u8>) -> anyhow::Result<T> {
+        let sch_payload = self.verify()?.decrypt(ecies_priv_key)?;
+        let request_str = std::str::from_utf8(&sch_payload.request)
+            .context("Failed to convert request to UTF-8 string")?;
+
+        let payload: T = serde_json::from_str(request_str)
+            .context("Failed to deserialize request into payload")?;
+
+        Ok(payload)
+    }
+}
+
+impl SCHPayload {
+    fn verify(&self) -> anyhow::Result<Self> {
+        let response_key = self.response_key.clone();
+        let request = self.request.clone();
+
+        let tokens = vec![
+            Token::Bytes(request.clone()),
+            Token::Bytes(response_key.clone()),
+        ];
+
+        let encoded = encode(&tokens);
+
+        let digest = keccak256(&encoded);
+
+        use ethers::types::Signature;
+        use std::convert::TryFrom;
+
+        let signature = match Signature::try_from(&self.signature[..]) {
+            Ok(data) => data,
+            Err(e) => return Err(anyhow::Error::msg(e.to_string())),
+        };
+
+        let recovered_address = match signature.recover(digest) {
+            Ok(data) => data,
+            Err(e) => return Err(anyhow::Error::msg(e.to_string())),
+        };
+
+        use ethers::core::k256::elliptic_curve::sec1::ToEncodedPoint;
+        use ethers::core::k256::PublicKey as K256PublicKey;
+
+        let response_pubkey = match K256PublicKey::from_sec1_bytes(&response_key) {
+            Ok(data) => data,
+            Err(e) => return Err(anyhow::Error::msg(e.to_string())),
+        };
+
+        let response_pubkey_bytes = response_pubkey.to_encoded_point(false);
+        let response_pubkey_uncompressed = &response_pubkey_bytes.as_bytes()[1..];
+        let response_address = Address::from_slice(&keccak256(response_pubkey_uncompressed)[12..]);
+
+        if recovered_address == response_address {
+            return Ok(self.clone());
+        } else {
+            return Err(anyhow::Error::msg(
+                "Signature verification failed.".to_string(),
+            ));
+        }
+    }
+
+    fn decrypt(&self, ecies_private_key: &[u8]) -> anyhow::Result<Self> {
+        let request = match decrypt_ecies(ecies_private_key, &self.request) {
+            Ok(data) => data,
+            Err(e) => return Err(anyhow::Error::msg(e.to_string())),
+        };
+
+        Ok(SCHPayload {
+            request,
+            response_key: self.response_key.clone(),
+            signature: self.signature.clone(),
+        })
+    }
+}
+
+#[derive(Serialize, Debug, Clone, Deserialize)]
+pub struct SCHResponse {
+    response: Vec<u8>,
+}
+
+pub trait ToSchResponse {
+    fn to_sch_response(&self, response_payload: Value) -> anyhow::Result<SCHResponse>;
+}
+
+impl ToSchResponse for SCHPayload {
+    fn to_sch_response(&self, response_payload: Value) -> anyhow::Result<SCHResponse> {
+        let json_str = serde_json::to_string(&response_payload)
+            .context("Failed to serialize response payload to JSON")?;
+
+        let utf8_bytes = json_str.as_bytes().to_vec();
+
+        let encrypted_data = match encrypt_ecies(&utf8_bytes, &self.response_key) {
+            Ok(data) => data,
+            Err(e) => return Err(anyhow::Error::msg(e.to_string())),
+        };
+
+        Ok(SCHResponse {
+            response: encrypted_data,
+        })
+    }
+}
 
 #[derive(Serialize, Debug, Validate, Deserialize)]
 pub struct SignAddress {
@@ -49,18 +169,65 @@ async fn sign_address(
             Some(Value::String(err.to_string())),
         );
     }
-    let addy_to_be_signed = json_input.address.as_ref().unwrap();
+
+    let ecies_priv_key = { ecies_priv_key.lock().unwrap().clone() };
+
+    let signed_address = _sign_address(json_input, &ecies_priv_key).await;
+
+    if signed_address.is_some() {
+        response("Address signed", StatusCode::OK, signed_address)
+    } else {
+        response("Address signing failed", StatusCode::BAD_REQUEST, None)
+    }
+}
+
+// Sign Address Encrypted
+#[post("/signAddressEncrypted")]
+async fn sign_address_encrypted(
+    jsonbody: web::Json<SCHPayload>,
+    ecies_priv_key: Data<Arc<Mutex<Vec<u8>>>>,
+) -> impl Responder {
     let ecies_priv_key = {
         let key = ecies_priv_key.lock().unwrap().clone();
-        hex::encode(key)
+        key
     };
+
+    let json_input: SignAddress = match jsonbody.0.to_payload(&ecies_priv_key) {
+        Ok(data) => data,
+        Err(e) => return response(&e.to_string(), StatusCode::BAD_REQUEST, None),
+    };
+
+    let signed_address = _sign_address(&json_input, &ecies_priv_key).await;
+
+    if signed_address.is_some() {
+        let signed_address = signed_address.unwrap();
+        let sch_response = match jsonbody.0.to_sch_response(signed_address) {
+            Ok(data) => data,
+            Err(e) => return response(&e.to_string(), StatusCode::BAD_REQUEST, None),
+        };
+
+        response(
+            "Address signed",
+            StatusCode::OK,
+            Some(serde_json::to_value(&sch_response).unwrap()),
+        )
+    } else {
+        response("Address signing failed", StatusCode::BAD_REQUEST, None)
+    }
+}
+
+async fn _sign_address(body: &SignAddress, ecies_priv_key: &Vec<u8>) -> Option<Value> {
+    let addy_to_be_signed = body.address.as_ref().unwrap();
+    let ecies_priv_key = hex::encode(ecies_priv_key);
     let signed = sign_addy(ecies_priv_key, addy_to_be_signed).await.unwrap();
     let signature = json!({
         "r": ethers::types::H256::from_uint(&signed.r),
         "s": ethers::types::H256::from_uint(&signed.s),
         "v": signed.v
     });
-    response("Address signed", StatusCode::OK, Some(signature))
+
+    Some(signature)
+    // response("Address signed", StatusCode::OK, )
 }
 
 // Sign Attestaion
--- matching_engine_client/src/main.rs
+++ matching_engine_client/src/main.rs
@@ -10,11 +10,15 @@ async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
 
     let port: u16 = env::var("PORT")
-        .unwrap_or_else(|_| "1500".to_string())
+        .unwrap_or_else(|_| "5000".to_string())
         .parse::<u16>()
         .expect("PORT must be a valid number");
 
-    let enclave_key = fs::read("/app/secp.sec").await?;
+    let enclave_key = match fs::read("/app/secp.sec").await {
+        Ok(key) => key,
+        Err(_) => fs::read("./app/secp.sec").await?,
+    };
+
     let server = client::MatchingEngineClient::new(hex::encode(enclave_key), port);
 
     server.start(false).await.unwrap();
