c969fc3d8c1a4402fd6d6df91f82bcae0f24e3b6
Akshay <akshay_111meher@hotmail.com>
2024-10-22 16:37:42 +0000

    simply cleanup

--- matching_engine/src/jobs/cleanup.rs
+++ matching_engine/src/jobs/cleanup.rs
@@ -4,10 +4,10 @@ use std::{
         atomic::{AtomicBool, Ordering},
         Arc,
     },
+    thread,
     time::Duration,
 };
 use tokio::sync::RwLock;
-use tokio::time::interval;
 
 use crate::ask_lib::ask_store::LocalAskStore;
 
@@ -17,7 +17,7 @@ pub struct CleanupTool {
     should_stop: Arc<AtomicBool>,
     ask_store: Arc<RwLock<LocalAskStore>>,
     #[allow(unused)]
-    proof_market_place: ProofMarketplaceInstance, // will be used later for more finely cleaning up services,
+    proof_market_place: ProofMarketplaceInstance, // will be used latter for more finely cleaning up services,
     relayer_address: Address,
     relayer_address_balance: Arc<RwLock<ethers::types::U256>>,
 }
@@ -39,77 +39,49 @@ impl CleanupTool {
         }
     }
 
-    /// Starts the cleanup task. This function spawns a background task that periodically
-    /// cleans up the ask store and updates the relayer balance.
-    pub fn start_cleanup(
+    pub async fn ask_store_cleanup(
         self,
         skip_relayer_balance_check: bool,
         slow_cleanup: bool,
-    ) -> tokio::task::JoinHandle<anyhow::Result<()>> {
-        tokio::spawn(async move {
-            // Determine the interval duration based on the `slow_cleanup` flag
-            let cleanup_interval = if slow_cleanup {
-                Duration::from_secs(30)
+    ) -> anyhow::Result<()> {
+        loop {
+            if slow_cleanup {
+                thread::sleep(Duration::from_secs(30));
             } else {
-                Duration::from_secs(2)
-            };
-            let mut interval = interval(cleanup_interval);
-
-            loop {
-                tokio::select! {
-                    _ = interval.tick() => {
-                        // Perform the cleanup operations
-
-                        if skip_relayer_balance_check {
-                            // Update the relayer's balance
-                            let balance = match self
-                                .proof_market_place
-                                .client()
-                                .get_balance(self.relayer_address, None)
-                                .await
-                            {
-                                Ok(data) => data,
-                                Err(err) => {
-                                    log::error!("Failed to get balance: {:?}", err);
-                                    ethers::types::U256::zero()
-                                },
-                            };
-                            {
-                                let mut relayer_balance_lock = self.relayer_address_balance.write().await;
-                                *relayer_balance_lock = balance;
-                                // Lock is automatically dropped here
-                            }
-                        }
-
-                        // Clean up completed asks
-                        {
-                            if let Some(completed_asks) = self.ask_store.read().await.get_cleanup_asks().result() {
-                                if completed_asks.len() > 0 {
-                                    let mut ask_store = self.ask_store.write().await;
-                                    for elem in completed_asks {
-                                        log::info!("Removed Completed ask: {}", &elem.ask_id);
-                                        ask_store.remove_ask_only_if_completed(&elem.ask_id);
-                                    }
-                                }
-                            }
-                            // Lock is automatically dropped here
-                        }
-                    }
-
-                    // Check if a shutdown has been requested
-                    _ = tokio::time::sleep(Duration::from_millis(100)) => {
-                        if self.should_stop.load(Ordering::Acquire) {
-                            log::info!("Gracefully shutting down cleanup task...");
-                            break;
-                        }
-                    }
-                }
-
-                // Optional: Yield to allow other tasks to run
-                tokio::task::yield_now().await;
+                thread::sleep(Duration::from_secs(1));
             }
 
-            Ok(())
-        })
+            if self.should_stop.load(Ordering::Acquire) {
+                log::info!("Gracefully shutting down...");
+                break;
+            }
+
+            if skip_relayer_balance_check {
+                let balance = match self
+                    .proof_market_place
+                    .client()
+                    .get_balance(self.relayer_address, None)
+                    .await
+                {
+                    Ok(data) => data,
+                    Err(_) => ethers::types::U256::zero(),
+                };
+                {
+                    *self.relayer_address_balance.write().await = balance;
+                }
+            }
+
+            if let Some(completed_asks) = self.ask_store.read().await.get_cleanup_asks().result() {
+                if completed_asks.len() > 0 {
+                    let mut ask_store = self.ask_store.write().await;
+                    for elem in completed_asks {
+                        log::info!("Removed Completed ask: {}", &elem.ask_id);
+                        ask_store.remove_ask_only_if_completed(&elem.ask_id);
+                    }
+                }
+            }
+        }
+
+        Ok(())
     }
 }
