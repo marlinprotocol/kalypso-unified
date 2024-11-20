12793937dc8b16bf9c7c126ccc35ed1adee76a06
Akshay <akshay_111meher@hotmail.com>
2024-11-20 12:28:50 +0000

    builds for pipeline

--- build-amd64.sh
+++ build-amd64.sh
@@ -10,4 +10,6 @@ apt-get install -y musl-dev musl-tools musl-gcc
 
 rustup target add x86_64-unknown-linux-musl
 
+export CC_x86_64_unknown_linux_musl=musl-gcc
+
 cargo build --release --target x86_64-unknown-linux-musl
--- build-arm64.sh
+++ build-arm64.sh
@@ -10,4 +10,12 @@ apt-get install -y musl-dev musl-tools gcc-aarch64-linux-musl
 
 rustup target add aarch64-unknown-linux-musl
 
+wget https://musl.cc/aarch64-linux-musl-gcc-12.2.0-x86_64-linux-musl.tar.gz
+
+tar -xzf aarch64-linux-musl-gcc-12.2.0-x86_64-linux-musl.tar.gz -C /usr/local
+
+export PATH=/usr/local/aarch64-linux-musl-gcc-12.2.0-x86_64-linux-musl/bin:$PATH
+
+export CC_aarch64_unknown_linux_musl=aarch64-linux-musl-gcc
+
 cargo build --release --target aarch64-unknown-linux-musl
