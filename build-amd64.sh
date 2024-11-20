5a0fc42ba2d0aea94bd5c2a3894f0954efc5ca9c
Akshay <akshay_111meher@hotmail.com>
2024-11-20 12:43:37 +0000

    builds for pipeline

--- build-amd64.sh
+++ build-amd64.sh
@@ -6,10 +6,15 @@ set -e
 
 cd /code
 
-apt-get install -y musl-dev musl-tools musl-gcc
+apt-get update && apt-get install -y --no-install-recommends \
+    musl-dev \
+    musl-tools \
+    musl-gcc
 
+# Add the musl target for Rust
 rustup target add x86_64-unknown-linux-musl
 
+# Set the compiler for the musl target
 export CC_x86_64_unknown_linux_musl=musl-gcc
 
 cargo build --release --target x86_64-unknown-linux-musl
