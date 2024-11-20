43eaff26101b37cf044e5f252e21882ae94b3c22
Akshay <akshay_111meher@hotmail.com>
2024-11-20 13:03:44 +0000

    builds for pipeline

--- build-amd64.sh
+++ build-amd64.sh
@@ -6,15 +6,6 @@ set -e
 
 cd /code
 
-apt-get update && apt-get install -y --no-install-recommends \
-    musl-dev \
-    musl-tools \
-    musl-gcc
-
-# Add the musl target for Rust
 rustup target add x86_64-unknown-linux-musl
-
-# Set the compiler for the musl target
-export CC_x86_64_unknown_linux_musl=musl-gcc
 
 cargo build --release --target x86_64-unknown-linux-musl
--- build-arm64.sh
+++ build-arm64.sh
@@ -6,26 +6,6 @@ set -e
 
 cd /code
 
-apt-get update && apt-get install -y --no-install-recommends \
-    musl-dev \
-    musl-tools \
-    gcc-aarch64-linux-musl \
-    wget \
-    tar
-
-# Add the Rust target for aarch64 with musl
 rustup target add aarch64-unknown-linux-musl
-
-# Download and extract the musl cross-compilation toolchain
-MUSL_TOOLCHAIN_URL="https://musl.cc/aarch64-linux-musl-gcc-12.2.0-x86_64-linux-musl.tar.gz"
-wget "$MUSL_TOOLCHAIN_URL" -O /tmp/aarch64-linux-musl-gcc.tar.gz
-tar -xzf /tmp/aarch64-linux-musl-gcc.tar.gz -C /usr/local
-rm /tmp/aarch64-linux-musl-gcc.tar.gz
-
-# Add the musl toolchain to the PATH
-export PATH=/usr/local/aarch64-linux-musl-gcc-12.2.0-x86_64-linux-musl/bin:$PATH
-
-# Set the musl cross-compiler for the target
-export CC_aarch64_unknown_linux_musl=aarch64-linux-musl-gcc
 
 cargo build --release --target aarch64-unknown-linux-musl
