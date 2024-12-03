84d5ccc2d4ffe4fd1c0e8fc2af7c39d9f4d36ed0
Akshay <akshay_111meher@hotmail.com>
2024-12-03 08:21:10 +0000

    Enable CLI to update metadata

---
+++ kalypso-cli/src/operations/update_generator_meta.rs
@@ -0,0 +1,57 @@
+use std::{collections::HashMap, fs, io};
+
+use async_trait::async_trait;
+
+use crate::common_deps::CommonDeps;
+
+use super::Operation;
+
+pub struct UpdateGeneratorMeta;
+
+#[async_trait]
+impl Operation for UpdateGeneratorMeta {
+    async fn execute(&self, config: HashMap<String, String>) -> Result<(), String> {
+        let generator_meta_info = CommonDeps::update_generator_meta_info(&config)?;
+
+        let generator_meta_paths = ["./generatormeta.json"];
+
+        let data = read_file_from_paths(&generator_meta_paths)
+            .map_err(|e| format!("Failed reading generatormeta.json {}", e))?;
+
+        let market_meta: matching_engine_helpers::generator_lib::generator_store::GeneratorMeta =
+            serde_json::from_str(&data).map_err(|e| {
+                format!("Failed deserde generatormeta.json into GeneratorMeta {}", e)
+            })?;
+
+        let json_string = serde_json::to_string(&market_meta)
+            .map_err(|e| format!("Failed converting GeneratorMeta to string{}", e))?;
+
+        let generator_metadata: Vec<u8> = json_string.into_bytes();
+
+        let tx_hash = CommonDeps::send_and_confirm(
+            generator_meta_info
+                .generator_registry
+                .update_generator_data(generator_metadata.into())
+                .send(),
+        )
+        .await?;
+
+        // Print the transaction hash
+        println!("Update Generator Metadata Transaction: {}", tx_hash);
+
+        Ok(())
+    }
+}
+
+// Helper function to read a file from multiple possible paths
+pub fn read_file_from_paths(paths: &[&str]) -> io::Result<String> {
+    for path in paths {
+        if let Ok(content) = fs::read_to_string(path) {
+            return Ok(content);
+        }
+    }
+    Err(io::Error::new(
+        io::ErrorKind::NotFound,
+        "File not found in any of the specified paths",
+    ))
+}
---
+++ kalypso-cli/src/operations/update_market_metadata.rs
@@ -0,0 +1,56 @@
+use std::{collections::HashMap, fs, io};
+
+use async_trait::async_trait;
+
+use crate::common_deps::CommonDeps;
+
+use super::Operation;
+
+pub struct UpdateMarketMetadata;
+
+#[async_trait]
+impl Operation for UpdateMarketMetadata {
+    async fn execute(&self, config: HashMap<String, String>) -> Result<(), String> {
+        let market_meta_update_info = CommonDeps::update_market_metadata_info(&config)?;
+
+        let meta_paths = ["./marketmeta.json"];
+
+        let data = read_file_from_paths(&meta_paths)
+            .map_err(|e| format!("Failed reading marketmeta.json {}", e))?;
+
+        let market_meta: matching_engine_helpers::market_metadata::MarketMetadata =
+            serde_json::from_str(&data)
+                .map_err(|e| format!("Failed deserde marketmeta.json into MarketMetadata{}", e))?;
+
+        let json_string = serde_json::to_string(&market_meta)
+            .map_err(|e| format!("Failed converting MarketMetadata to string{}", e))?;
+
+        let market_metadata: Vec<u8> = json_string.into_bytes();
+
+        let tx_hash = CommonDeps::send_and_confirm(
+            market_meta_update_info
+                .proof_marketplace
+                .update_market_metadata(market_meta_update_info.market_id, market_metadata.into())
+                .send(),
+        )
+        .await?;
+
+        // Print the transaction hash
+        println!("Update Market Metadata Transaction: {}", tx_hash);
+
+        Ok(())
+    }
+}
+
+// Helper function to read a file from multiple possible paths
+pub fn read_file_from_paths(paths: &[&str]) -> io::Result<String> {
+    for path in paths {
+        if let Ok(content) = fs::read_to_string(path) {
+            return Ok(content);
+        }
+    }
+    Err(io::Error::new(
+        io::ErrorKind::NotFound,
+        "File not found in any of the specified paths",
+    ))
+}
