04034abae4ce465c70e47fa1bf5e5f0ef6e856e1
Akshay <akshay_111meher@hotmail.com>
2025-01-21 12:15:19 +0000

    update

--- matching_engine/src/routes/ui_routes/single_market.rs
+++ matching_engine/src/routes/ui_routes/single_market.rs
@@ -368,7 +368,7 @@ async fn recompute_single_market_response<'a>(
                             token: address_to_string(&USDC_TOKEN),
                             amount: a.reward.to_string(),
                         },
-                        time: (a.deadline.saturating_sub(a.created_on)).to_string(),
+                        time: a.time_requested_for_proof_generation.to_string(),
                         inputs: a.prover_data.to_string(),
                         generator: None,
                         status: AskState::Create,
