a3d2f438a04f405c618c822147f68dce260d7420
Akshay <akshay_111meher@hotmail.com>
2025-02-25 13:23:37 +0400

    skip balance checks in generator client

--- generator_client/src/kalypso.rs
+++ generator_client/src/kalypso.rs
@@ -63,7 +63,7 @@ pub async fn generate_config_file(
     ecies_private_key: String,
 ) -> Result<(), Box<dyn std::error::Error>> {
     // Create config_file_folder
-    let folder_path = "../generator_config";
+    let folder_path = "./generator_config";
     if fs::metadata(&folder_path).await.is_ok() {
         log::info!("generator_config folder already exists!");
     } else {
@@ -99,7 +99,7 @@ pub async fn generate_runtime_file(
     runtime_config_body: &SetupRequestBodyRuntimeConfig,
 ) -> Result<(), Box<dyn std::error::Error>> {
     // Create config_file_folder
-    let folder_path = "../generator_config";
+    let folder_path = "./generator_config";
     if fs::metadata(&folder_path).await.is_ok() {
         log::info!("generator_config folder already exists!");
     } else {
@@ -193,7 +193,7 @@ pub async fn update_runtime_config_with_new_data(
 
 //Read the runtime config file
 pub async fn read_runtime_config_file() -> Result<RuntimeConfigFile, Box<dyn std::error::Error>> {
-    let file = File::open("../generator_config/runtime_config.json").await?;
+    let file = File::open("./generator_config/runtime_config.json").await?;
     let mut buf_reader = tokio::io::BufReader::new(file);
 
     let mut content = String::new();
@@ -208,7 +208,7 @@ pub async fn update_runtime_config_file(
     generator_config: RuntimeConfigFile,
 ) -> Result<(), std::io::Error> {
     let json_string = serde_json::to_string(&generator_config)?;
-    let mut file = File::create("../generator_config/runtime_config.json").await?;
+    let mut file = File::create("./generator_config/runtime_config.json").await?;
     tokio::io::AsyncWriteExt::write_all(&mut file, json_string.as_bytes()).await?;
     Ok(())
 }
@@ -216,7 +216,7 @@ pub async fn update_runtime_config_file(
 //Read the generator config file
 pub async fn read_generator_config_file() -> Result<GeneratorConfigFile, Box<dyn std::error::Error>>
 {
-    let file = File::open("../generator_config/generator_config.json").await?;
+    let file = File::open("./generator_config/generator_config.json").await?;
     let mut buf_reader = tokio::io::BufReader::new(file);
 
     let mut content = String::new();
@@ -250,7 +250,7 @@ pub async fn update_generator_config_file(
     generator_config: GeneratorConfigFile,
 ) -> Result<(), std::io::Error> {
     let json_string = serde_json::to_string(&generator_config)?;
-    let mut file = File::create("../generator_config/generator_config.json").await?;
+    let mut file = File::create("./generator_config/generator_config.json").await?;
     tokio::io::AsyncWriteExt::write_all(&mut file, json_string.as_bytes()).await?;
     Ok(())
 }
